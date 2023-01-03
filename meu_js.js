3188cfaacc21f90d118112a3153f7d30dfaea612
Fed2001-kmf <112756471+Fed2001-kmf@users.noreply.github.com>
2023-01-03 00:00:51 -0300

    Minha pasta preferida

---
+++ meu_js.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é o seu nome?')
+    window.alert ('Seja muito bem-vindo(a) meu querido(a) ' + nome)
+    window.prompt ('Quer visitar o meu site? ' + nome)
+    window.confirm ('Então show!!!! Bora começar navegar ' + nome)
